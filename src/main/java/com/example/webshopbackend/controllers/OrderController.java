f572009c906cc664ffa5c4bc6ebda231dbfd02a6
Tomas Dahlander <tomas.dahlander@gmail.com>
2021-04-12 11:10:36 +0200

    Added all models and DAO classes and skeleton for controllers

---
+++ src/main/java/com/example/webshopbackend/controllers/OrderController.java
@@ -0,0 +1,10 @@
+package com.example.webshopbackend.controllers;
+
+/**
+ * Created by Tomas Dahlander <br>
+ * Date: 2021-04-12 <br>
+ * Time: 11:09 <br>
+ * Project: webshop-back-end <br>
+ */
+public class OrderController {
+}
